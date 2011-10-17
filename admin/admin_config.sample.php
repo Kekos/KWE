a591e156153c4f077e057c00ba45252aebea82b5
Christoffer Lindahl <christoffer@kekos.se>
2011-10-17 19:44:23 +0200

    Removed my configuration files and added sample configuration files instead.

--- admin/admin_config.sample.php
+++ admin/admin_config.sample.php
@@ -2,10 +2,10 @@
 define('DEBUG', ($_SERVER['REMOTE_ADDR'] == '127.0.0.1'));
 
 define('MySQL_SERVER', 'localhost');
-define('MySQL_USER', 'admincl');
-define('MySQL_PASSWORD', 'smultron');
-define('MySQL_DB', 'kwe');
-define('DB_PREFIX', 'kwe');
+define('MySQL_USER', 'user');
+define('MySQL_PASSWORD', 'password');
+define('MySQL_DB', 'db_name');
+define('DB_PREFIX', '');
 
 define('RESPONSE_LAYOUT', 'view/admin/layout.phtml');  // The main template file in which other templates will be included in
 define('ERROR_LAYOUT', 'view/admin/error_layout.phtml');  // The error template file in which other error templates will be included in
@@ -13,11 +13,12 @@ define('ERROR_LAYOUT', 'view/admin/error_layout.phtml');  // The error template 
 define('BASE', '../');  // Base server path relative to admin interface, should end with / if not empty
 define('BASE_SITE', '');  // Base server path, should end with / if not empty
 define('FULLPATH', '/admin');  // Full server path to admin root, should not end with /
-define('FULLURL', 'http://kwe/admin');  // Full URL to admin root, should not end with /
+define('FULLURL', 'http://domain/admin');  // Full URL to admin root, should not end with /
 define('FULLPATH_SITE', '');  // Full server path to site root, should not end with /
-define('FULLURL_SITE', 'http://kwe');  // Full URL to site root, should not end with /
+define('FULLURL_SITE', 'http://domain');  // Full URL to site root, should not end with /
+define('ERROR_LOG_PATH', '');  // If empty KWF logs errors to your root (BASE). Don't add the value of BASE to this!
 
 define('MOD_REWRITE', 1);
 define('SESSION_PREFIX', 'kwe');
-define('ERROR_MAIL', 'christoffer[snabel-a]kekos[punkt]se');
+define('ERROR_MAIL', 'your[at]mail[dot]net');
 ?>
--- config.sample.php
+++ config.sample.php
@@ -2,19 +2,20 @@
 define('DEBUG', ($_SERVER['REMOTE_ADDR'] == '127.0.0.1'));
 
 define('MySQL_SERVER', 'localhost');
-define('MySQL_USER', 'admincl');
-define('MySQL_PASSWORD', 'smultron');
-define('MySQL_DB', 'kwe');
-define('DB_PREFIX', 'kwe');
+define('MySQL_USER', 'user');
+define('MySQL_PASSWORD', 'password');
+define('MySQL_DB', 'db_name');
+define('DB_PREFIX', '');
 
 define('RESPONSE_LAYOUT', 'view/layout.phtml');  // The main template file in which other templates will be included in
 define('ERROR_LAYOUT', 'view/error_layout.phtml');  // The error template file in which other error templates will be included in
 
 define('BASE', '');  // Base server path, should end with / if not empty
 define('FULLPATH', '');  // Full server path to root, should not end with /
-define('FULLURL', 'http://kwe');  // Full URL to root, should not end with /
+define('FULLURL', 'http://domain');  // Full URL to root, should not end with /
+define('ERROR_LOG_PATH', '');  // If empty KWF logs errors to your root (BASE). Don't add the value of BASE to this!
 
 define('MOD_REWRITE', 1);
 define('SESSION_PREFIX', 'kwe');
-define('ERROR_MAIL', 'christoffer[snabel-a]kekos[punkt]se');
+define('ERROR_MAIL', 'your[at]mail[dot]net');
 ?>
