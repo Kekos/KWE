606bf54a51f624a9578fd869fe7e282c55da67e7
Christoffer Lindahl <christoffer@kekos.se>
2012-10-12 12:26:16 +0200

    Made new Controller class for KWE controllers using EventListener because the Controller constructor must call EventListener constructor.

---
+++ class/EventController.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * KWF Class: EventController, the generalized controller class. Must be extended
+ * 
+ * @author Christoffer Lindahl <christoffer@kekos.se>
+ * @date 2012-10-12
+ * @version 1.0
+ */
+
+abstract class EventController extends EventListener
+  {
+  protected $request;
+  protected $response;
+  protected $page;
+  protected $controller_data;
+  protected $route;
+  protected $params;
+  protected $view;
+
+  /*
+   * Constructor: EventController
+   *
+   * @param object $request The request object that created the instance
+   * @param object $response The response object used to make output
+   * @param object $page The page object
+   * @param array $controller_data An array with data associated to controller
+   * @param string $route Contains the route to page which started this controller
+   * @param string $params Contains the params sent to page which started this controller
+   * @return void
+   */
+	public function __construct($request, $response, $page, $controller_data, $route, $params)
+    {
+    parent::__construct();
+
+    $this->request = $request;
+    $this->response = $response;
+    $this->page = $page;
+    $this->controller_data = $controller_data;
+    $this->route = $route;
+    $this->params = $params;
+    }
+
+  /*
+   * Runs the controller (set the view template file, set response content type and add content to response)
+   *
+   * @return void
+   */
+	abstract public function run();
+  }
+?>
